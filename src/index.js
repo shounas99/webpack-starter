f022557115f1aa807db78ea168d6f1b3be15fa97
Cristina Aguilar <17030674@itcelaya.edu.mx>
2021-02-25 20:41:16 -0600

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { saludar } from './js/componentes.js'; //importacion del modulo, adentro se pone el nombre de la funcion
+import './styles.css';
+
+
+const nombre = 'Cris!';
+
+saludar(nombre);
